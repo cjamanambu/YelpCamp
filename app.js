28fed7747557e2ee41518004aab389d09398e30f
CJ Amanambu <oamanambu@yahoo.com>
2018-12-28 16:10:31 -0500

    Version 4 YelpCamp Commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express     = require('express'),
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get('/', function(req, res){
@@ -31,9 +32,9 @@ app.get('/campgrounds', function(req, res){
 
 // create route
 app.post('/campgrounds', function(req, res){
-  var name = req.body.name;
+  var name  = req.body.name;
   var image = req.body.image;
-  var desc = req.body.description;
+  var desc  = req.body.description;
   var newCampground = {name: name, image: image, description: desc};
   // create a new campground and save to db
   Campground.create(newCampground, function(err, newCreated){
--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ var mongoose   = require("mongoose"),
         {
             name: "Cloud's Rest",
             image: "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            description: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae"
+            description: "Maecenas viverra metus purus, pharetra gravida lacus venenatis at. Curabitur porttitor ac nisi a gravida. Vivamus aliquet a massa in cursus. Morbi mattis arcu malesuada fermentum tincidunt. Suspendisse potenti. Pellentesque ac faucibus ipsum. Cras lacinia et urna ac ullamcorper. Cras dolor dui, auctor at odio ac, finibus congue risus. Sed sit amet sapien in libero consectetur vestibulum. Aliquam nulla mi, fringilla et vestibulum at, viverra quis lacus. Aenean accumsan porttitor felis, vel mattis turpis. Suspendisse potenti."
         },
         {
             name: "Desert Mesa",
             image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            description: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae"
+            description: "Maecenas viverra metus purus, pharetra gravida lacus venenatis at. Curabitur porttitor ac nisi a gravida. Vivamus aliquet a massa in cursus. Morbi mattis arcu malesuada fermentum tincidunt. Suspendisse potenti. Pellentesque ac faucibus ipsum. Cras lacinia et urna ac ullamcorper. Cras dolor dui, auctor at odio ac, finibus congue risus. Sed sit amet sapien in libero consectetur vestibulum. Aliquam nulla mi, fringilla et vestibulum at, viverra quis lacus. Aenean accumsan porttitor felis, vel mattis turpis. Suspendisse potenti."
         },
         {
             name: "Canyon Floor",
             image: "https://images.unsplash.com/photo-1519708495087-ca1b71df408c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-            description: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae"
+            description: "Maecenas viverra metus purus, pharetra gravida lacus venenatis at. Curabitur porttitor ac nisi a gravida. Vivamus aliquet a massa in cursus. Morbi mattis arcu malesuada fermentum tincidunt. Suspendisse potenti. Pellentesque ac faucibus ipsum. Cras lacinia et urna ac ullamcorper. Cras dolor dui, auctor at odio ac, finibus congue risus. Sed sit amet sapien in libero consectetur vestibulum. Aliquam nulla mi, fringilla et vestibulum at, viverra quis lacus. Aenean accumsan porttitor felis, vel mattis turpis. Suspendisse potenti."
         }
 
     ];
